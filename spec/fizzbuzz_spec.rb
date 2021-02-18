a7ae6fdd9b7a020abb27c01cddf74c5b8e869549
loushark <lou.m.reade@gmail.com>
2021-02-18 15:12:17 +0000

    added if statement to return 4 and added test to return buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   it 'returns 4 when passed 4' do
-    expect(fizzbuzz(4)).to eq 4  
+    expect(fizzbuzz(4)).to eq 4
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
